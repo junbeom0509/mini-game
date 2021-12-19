cda097ff069974d730b4aef22e81d21b31b60629
nurimeansworld <nurimeansworld@gmail.com>
2021-12-19 21:28:34 +0900

    REF btn 클릭 이벤트 foreach로 변경

--- Q1/nrpark/script.js
+++ Q1/nrpark/script.js
@@ -30,28 +30,29 @@ let index = 0;
 const runGame = setInterval(function () {
   randVal.innerHTML = arr[index++];
   if (index == arr.length) index = 0;
-}, 1000);
+}, 500);
 
-let btn_scissor = document.querySelector(".btn_scissor");
-let btn_rock = document.querySelector(".btn_rock");
-let btn_paper = document.querySelector(".btn_paper");
-
-let btn_restart = document.querySelector(".btn_restart");
-
-btn_scissor.addEventListener("click", function () {
-  clearInterval(runGame);
-  checkGame(randVal.innerHTML, 0);
-});
-btn_rock.addEventListener("click", function () {
-  clearInterval(runGame);
-  checkGame(randVal.innerHTML, 1);
-});
-btn_paper.addEventListener("click", function () {
-  clearInterval(runGame);
-  checkGame(randVal.innerHTML, 2);
+const btn = document.querySelectorAll(".btn");
+btn.forEach(function (element) {
+  element.addEventListener("click", function () {
+    switch (this.innerHTML) {
+      case "바위":
+        userVal = "1";
+        break;
+      case "보":
+        userVal = "2";
+        break;
+      case "가위":
+        userVal = "0";
+        break;
+    }
+    clearInterval(runGame);
+    checkGame(randVal.innerHTML, userVal);
+  });
 });
 
 // CHECK:: 아직 구현 못함 ㅠ
+let btn_restart = document.querySelector(".btn_restart");
 btn_paper.addEventListener("click", function () {
   runGame = runGame;
 });
